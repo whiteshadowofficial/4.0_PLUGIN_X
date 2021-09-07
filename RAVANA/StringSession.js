d1135d889ba4bf7e8307332902728dde92efb64f
SL GEEK BOY <89677013+agentmax123@users.noreply.github.com>
2021-09-07 10:59:26 +0530

    Update StringSession.js

--- RAVANA/StringSession.js
+++ RAVANA/StringSession.js
@@ -28,7 +28,7 @@ class StringSession {
     }
 
     createStringSession(dict) {
-        return 'RAVANA:::' + Buffer.from(JSON.stringify(dict)).toString('base64');
+        return 'RAVANA;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
     }
 }
 
